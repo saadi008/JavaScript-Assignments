e60f26df50ca7928966b48e5d6742b8adb8d2507
Saad Ali <saadiali0080@gmail.com>
2024-02-24 18:37:43 +0500

    chapter-2 Q.no 3 done

--- Chapter-2/app.js
+++ Chapter-2/app.js
@@ -9,3 +9,11 @@ alert (username)
 
 var myName = ("Muhammad Saad Ali")
 alert (myName)
+
+/* Q)3. Write script to
+a) Declare a JS variable, titled message.
+b) Assign “Hello World” to variable message
+c) Display the message in alert box.*/
+
+var message = ("Hello World")
+alert (message)
