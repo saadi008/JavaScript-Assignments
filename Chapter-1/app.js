d0a5981c6f36bf715b7cb5227d09d9daf3751518
Saad Ali <saadiali0080@gmail.com>
2024-02-24 18:13:30 +0500

    Q . no 2 done

--- Chapter-1/app.js
+++ Chapter-1/app.js
@@ -2,3 +2,7 @@
 //Q)1. Write a script to greet your website visitor using JS alert box.
 
 alert("Wlcome to my Website")
+
+//Q)2. 2. Write a script to display following message on your web page:
+
+alert ("Error! Please enter a valid password.")
