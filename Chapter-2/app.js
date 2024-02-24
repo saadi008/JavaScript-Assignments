66aea02c4debe362201ce54e65d20419ced8c632
Saad Ali <saadiali0080@gmail.com>
2024-02-24 19:00:58 +0500

    chapter-2 Q.n0 7 done

--- Chapter-2/app.js
+++ Chapter-2/app.js
@@ -38,3 +38,11 @@ string concatenation) */
 
 var email = ("sa7101498@gmail.com")
 alert ("My Email Address is "+ email)
+
+
+/* Q) 7. Declare a variable called book & give it the value “A
+smarter way to learn JavaScript”. Display the following
+message in an alert box: */
+
+var book = ("A smarter way to learn JavaScript.")
+alert ("I am trying to learn from the book " + book)
