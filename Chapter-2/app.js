83797fa0510bddaaab0d6e30ebce16131be6ac3e
Saad Ali <saadiali0080@gmail.com>
2024-02-24 18:35:12 +0500

    chapter 2 q. no 2 done

--- Chapter-2/app.js
+++ Chapter-2/app.js
@@ -3,3 +3,9 @@
 
 var username = ("saadi")
 alert (username)
+
+
+// Q) 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+var myName = ("Muhammad Saad Ali")
+alert (myName)
