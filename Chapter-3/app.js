df0cc6ea394461324603f320c404350cc84311c8
Saad Ali <saadiali0080@gmail.com>
2024-02-26 14:14:27 +0500

    Chapter-3 Q .no 3 done

--- Chapter-3/app.js
+++ Chapter-3/app.js
@@ -29,6 +29,23 @@ sessionStorage.setItem('visits', numberOfVisits);
 alert("You have visited this site " + numberOfVisits + " times.");
 
 
+// Q) 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+
+
+// Declare a variable called birthYear and assign your birth year to it
+var birthYear = 2003;
+
+// Display a message in the browser with your birth year
+alert("My birth year is: " + birthYear);
+
+// Display the message on the web page
+document.write ("My birth year is: " + birthYear+ "<br>" + "Data type of my declared variable is number");
+
+
+
+
+
+
 
 
 
