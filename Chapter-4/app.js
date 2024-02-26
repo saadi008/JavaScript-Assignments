97970210d0084e96d93c93b142e96a27ab96c988
Saad Ali <saadiali0080@gmail.com>
2024-02-26 14:52:46 +0500

    Chapter-4 Q.no 1 done

---
+++ Chapter-4/app.js
@@ -0,0 +1,8 @@
+// Q)1. Declare 3 variables in one statement.
+
+var variable1, variable2, variable3;
+variable1 = "This is my first variable";
+variable2 = "This is my second variable";
+variable3 = "This is my third variable";
+alert(variable1 +  "\n" + variable2  + "\n"  + variable3);
+
