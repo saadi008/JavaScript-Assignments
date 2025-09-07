268c52b440f97669e77caad39eee915ec07663de
saadi008 <saadiali0080@gmail.com>
2025-09-07 15:34:26 +0500

    Array Prastice completed.

---
+++ Arrays-Prastice/app.js
@@ -0,0 +1,209 @@
+//             ✅ Easy Level (5 Questions)
+
+//Q.1 Ek array fruits = ["Apple", "Banana", "Mango"] banao.
+// push() ka use karke "Orange" add karo aur updated array print karo.
+
+// var fruits = ["Apple", "Banana", "Mango"];
+// document.write(fruits + "<br/>")
+
+// fruits.push("Orange");
+// document.write("Update array is: " + fruits);
+
+
+
+
+// Q.2 Ek array colors = ["Red", "Green", "Blue"] banao.
+// pop() ka use karke last color delete karo aur updated array print karo.
+
+// var colors = ["Red", "Green", "Blue"];
+// document.write(colors + "<br/>") ;
+
+// colors.pop();
+// document.write("Update array is: " + colors);
+
+
+
+
+// Q.3 Ek array numbers = [10, 20, 30, 40] banao.
+// shift() ka use karke first element remove karo aur updated array print karo.
+
+// var num = [10, 20, 30, 40];
+// console.log("Original Array is: " + num);
+
+// num.shift();
+// console.log("Update Array is: " + num);
+
+
+
+// Q.4 Ek array cities = ["Karachi", "Lahore"] banao.
+// unshift() ka use karke "Islamabad" aur "Quetta" start me add karo.
+
+// var cities = ["Karachi", "Lahore"];
+// console.log(cities);
+
+// cities.unshift("Islamabad", "Quetta");
+// console.log("Updated Array is: " + cities);
+
+
+
+// Q.5 Ek array languages = ["HTML", "CSS", "JavaScript"] banao.
+// Directly slice(1, 3) use karke ek new array banao jisme sirf "CSS" aur "JavaScript" ho.
+
+// var languages = ["HTML", "CSS", "JavaScript"];
+// console.log(languages);
+
+// var copyLanguages = languages.slice(1, 3);
+// console.log(copyLanguages);
+
+
+
+
+//                         ✅ Intermediate Level (5 Questions)
+
+
+// Q.1 Ek array students = ["Ali", "Saad", "Meer", "Babar"] banao.
+// splice() use karke "Saad" ke baad "Ahmed" insert karo.
+
+// var students = ["Ali", "Saad", "Meer", "Babar"];
+// console.log(students);
+
+// students.splice(2, 0, "Ahmed");
+// console.log(students);
+
+
+
+// Q.2 Ek array numbers = [100, 200, 300, 400, 500] banao.
+// splice() use karke middle ke 2 elements delete karo.
+
+// var num = [100, 200, 300, 400, 500];
+// console.log(num);
+
+// num.splice(2,2);
+// console.log(num);
+
+
+
+// Q.3 Ek array months = ["Jan", "Feb", "Mar", "Apr", "May"] banao.
+// slice() use karke sirf "Feb", "Mar", "Apr" wala new array banao.
+
+// var months = ["Jan", "Feb", "Mar", "Apr", "May"];
+// console.log(months);
+
+// var copyMonths = months.slice(1,4);
+// console.log(copyMonths);
+
+
+
+// Q.4 Ek array animals = ["Cat", "Dog", "Lion"] banao.a
+// push() aur shift() dono use karke ek element add aur ek remove karo.
+
+// var animals = ["Cat", "Dog", "Lion"];
+// console.log(animals);
+
+// animals.push("Parrot");
+// console.log(animals);
+
+// animals.shift();
+// console.log(animals);
+
+
+
+
+// Q.5 Ek array phones = ["iPhone", "Samsung", "Nokia"] banao.
+// unshift() se ek brand start me add karo, pop() se ek brand end se remove karo.
+
+// var phones = ["Iphone", "Samsung", "Nokia"];
+// console.log(phones);
+
+// phones.unshift("Redmi");
+// console.log(phones);
+
+// phones.pop();
+// console.log(phones);
+
+
+
+
+//                 ✅ Advanced Level (5 Questions)
+
+// Q.1 Ek array marks = [20, 40, 60, 80, 100] banao.
+// splice() use karke index 2 par "50" insert karo aur updated array print karo.
+
+// var marks = [20, 40, 60, 80, 100];
+// console.log(marks);
+
+// marks.splice(2, 0, 50);
+// console.log(marks);
+
+
+
+// Q.2 Ek array fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"] banao.
+// splice() use karke "Mango" aur "Orange" delete karo aur unki jagah "Pineapple" aur "Guava" add karo.
+
+// var fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+// console.log(fruits);
+
+// fruits.splice(2,2,"Pineapple", "Guava");
+// console.log(fruits);
+
+
+
+
+// Q.3 Ek array numbers = [5, 10, 15, 20, 25] banao.
+// slice() use karke ek new array banao jisme sirf last 3 numbers ho.
+
+// var num = [5, 10, 15, 20, 25];
+// console.log(num);
+
+// var copyNum = num.slice(2,5);
+// console.log(copyNum);
+
+
+
+// Q.4 Ek array skills = ["HTML", "CSS", "JavaScript", "React", "Node"] banao.
+// splice() use karke sirf "React" ko remove karo aur updated array print karo.
+
+// var skills = ["HTML", "CSS", "JavaScript", "React", "Node"];
+// console.log(skills);
+
+// skills.splice(3,1);
+// console.log(skills);
+
+
+
+// Q.5 Ek array queue = [] banao.
+// Pehle push() use karke 5 elements add karo.
+// Phir shift() ka use karke elements ek ek karke remove karo (FIFO style).
+
+// var queue = [];
+// queue.push("apple", "mango", "banana", "orange");
+// console.log(queue);
+
+// console.log("Out: " + queue.shift());
+// console.log("Out: " + queue.shift());
+// console.log("Out: " + queue.shift());
+// console.log("Out: " + queue.shift());
+
+// console.log("Final queue: " + queue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
